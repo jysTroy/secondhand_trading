5f1d2725b38448df1503bb4714fd08db6ac85394
juyonghyeon <jyh928578@gmail.com>
2025-05-27 18:23:20 +0900

    2일차 끝2

--- src/main/java/org/ourspring/member/validators/JoinValidator.java
+++ src/main/java/org/ourspring/member/validators/JoinValidator.java
@@ -1,61 +1,90 @@
 package org.ourspring.member.validators;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.ourspring.global.validators.MobileValidator;
-import org.ourspring.global.validators.PasswordValidator;
 import org.ourspring.member.controllers.RequestJoin;
-import org.ourspring.member.repositories.MemberRepository;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Component;
+import org.ourspring.member.controllers.RequestLogin;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Lazy
-@Component
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 @RequiredArgsConstructor
-public class JoinValidator implements Validator, PasswordValidator, MobileValidator {
+@RequestMapping("/member")
+public class MemberController {
 
-    private final MemberRepository repository;
+    private final Utils utils;
+    private final JoinValidator joinValidator;
+    private final JoinService joinService;
 
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return clazz.isAssignableFrom(RequestJoin.class);
+    @ModelAttribute("addCss")
+    public List<String> addCss() {
+        return List.of("member/style");
     }
 
-    @Override
-    public void validate(Object target, Errors errors) {
+    // 회원가입 양식
+    @GetMapping("/join")
+    public String join(@ModelAttribute RequestJoin form, Model model) {
+        commonProcess("join", model);
+
+        return utils.tpl("member/join");
+    }
+
+    // 회원가입 처리
+    @PostMapping("/join")
+    public String joinPs(@Valid RequestJoin form, Errors errors, Model model) {
+        commonProcess("join", model);
+
+        joinValidator.validate(form, errors);
+
         if (errors.hasErrors()) {
-            return;
-        }
-        /**
-         * 1. 이메일 중복 여부
-         * 2. 비밀번호 복잡성
-         * 3. 비밀번호 확인
-         * 4. 휴대폰번호 형식 검증
-         */
-        RequestJoin form = (RequestJoin) target;
-        String password = form.getPassword();
-        String confirmPassword = form.getConfirmPassword();
-        String mobile = form.getMobile();
-
-        // 1. 이메일 중복 여부
-        if (repository.existsByEmail(form.getEmail())) {
-            errors.rejectValue("email","Duplicated");
+            return utils.tpl("member/join");
         }
 
-        // 2. 비밀번호 복잡성
-        if (!checkAlpha(password, false) || !checkNumber(password) || !checkSpecialChars(password)) {
-            errors.rejectValue("password","Complexity");
+        joinService.process(form);
+
+        // 회원가입 성공시
+        return "redirect:/member/login";
+    }
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute RequestLogin form, Model model) {
+        commonProcess("login", model);
+
+        return utils.tpl("member/login");
+    }
+
+    /**
+     * 현재 컨트롤러의 공통 처리 부분
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "join";
+        String pageTitle = "";
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        if (mode.equals("join")) { // 회원 가입 공통 처리
+            addCommonScript.add("fileManager");
+            addScript.add("member/join");
+            pageTitle = utils.getMessage("회원가입");
+
+        } else if (mode.equals("login")) { // 로그인 공통 처리
+            pageTitle = utils.getMessage("로그인");
         }
 
-        // 3. 비밀번호 확인
-        if (!password.equals(confirmPassword)) {
-            errors.rejectValue("confirmPassword", "Mismatch");
-        }
-
-        // 4. 휴대폰번호형식 검증
-        if (!checkMobile(mobile)) {
-            errors.rejectValue("mobile", "Format");
-        }
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("pageTitle", pageTitle);
     }
 }
