5d0d54253a49ad7cb27880354f25fca6bed5733c
juyonghyeon <jyh928578@gmail.com>
2025-05-28 18:25:10 +0900

    임시 저장

---
+++ src/main/java/org/ourspring/global/exceptions/script/AlertException.java
@@ -0,0 +1,10 @@
+package org.ourspring.global.exceptions.script;
+
+import org.ourspring.global.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class AlertException extends CommonException {
+    public AlertException(String message, HttpStatus status) {
+        super(message, status);
+    }
+}
