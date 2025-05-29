3cb0c4fdf0895f50477ae7d7bd2241961415fc4d
juyonghyeon <jyh928578@gmail.com>
2025-05-29 16:23:03 +0900

    5/29_16:22

--- src/main/java/org/ourspring/global/configs/SecurityConfig.java
+++ src/main/java/org/ourspring/global/configs/SecurityConfig.java
@@ -11,6 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        /* 인증 설정 - 로그인, 로그아웃 S */
+        http.formLogin(c -> {
+            c.loginPage("/member/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/member/join");
+        });
+
+        /* 인증 설정 - 로그인, 로그아웃 E */
+
         return  http.build();
     }
 
--- src/main/java/org/ourspring/member/controllers/RequestLogin.java
+++ src/main/java/org/ourspring/member/controllers/RequestLogin.java
@@ -4,4 +4,7 @@ import lombok.Data;
 
 @Data
 public class RequestLogin {
+    private String email;
+    private String password;
+    private boolean autoLogin;
 }
