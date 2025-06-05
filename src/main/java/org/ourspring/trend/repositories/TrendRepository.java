9bfef5d77d55ea1b76eb1dca415bc4376aa5a7b7
juyonghyeon <jyh928578@gmail.com>
2025-06-05 13:22:12 +0900

    6/05_13:22

---
+++ src/main/java/org/ourspring/trend/repositories/TrendRepository.java
@@ -0,0 +1,7 @@
+package org.ourspring.trend.repositories;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface TrendRepository extends ListCrudRepository<org.ourspring.trend.entities.Trend, Long> {
+
+}
