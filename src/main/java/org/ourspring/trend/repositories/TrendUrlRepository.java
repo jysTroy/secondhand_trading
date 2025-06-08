770e1b7f04e77a58a18ae34c7d5e576e79223407
jysTroy <jys_troy@naver.com>
2025-06-08 21:20:54 +0900

    etc_html 수정

--- src/main/java/org/ourspring/trend/repositories/TrendUrlRepository.java
+++ src/main/java/org/ourspring/trend/repositories/TrendUrlRepository.java
@@ -1,5 +1,6 @@
 package org.ourspring.trend.repositories;
 
+import org.ourspring.trend.entities.TrendUrl;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface TrendUrlRepository extends ListCrudRepository<TrendUrl, String> {
