9b8f3aa1d932dabe437a66dd4bdf926a7ab038e4
yesung <jys_troy@naver.com>
2025-07-07 17:11:05 +0900

    .

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -7,7 +7,7 @@ commonLib.mapLib = {
         // 카카오 맵 SDK 자바스크립트가 추가된 상태가 아니라면 추가
         if (!document.getElementById("kakao-map-sdk")) {
             const scriptEl = document.createElement("script");
-            scriptEl.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=c353cbff16c035545fd3661e0c9019ed";
+            scriptEl.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=2c77b1985c176db589f739dcee2c37de";
             scriptEl.id = "kakao-map-sdk";
             headEl.prepend(scriptEl);
 
@@ -84,5 +84,5 @@ commonLib.mapLib = {
 };
 
 window.addEventListener("DOMContentLoaded", function() {
-   //commonLib.mapLib.init();
+//   commonLib.mapLib.init();
 });
