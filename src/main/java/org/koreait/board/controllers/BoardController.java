7ca6809fa5e46b3630342e43f80b1cc78a0f8971
yesung <jys_troy@naver.com>
2025-07-23 13:20:58 +0900

    .

--- src/main/java/org/koreait/board/controllers/BoardController.java
+++ src/main/java/org/koreait/board/controllers/BoardController.java
@@ -6,6 +6,7 @@ import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,18 +18,47 @@ public class BoardController {
 
     private final Utils utils;
 
+    // 게시글 목록
     @GetMapping("/list/{bid}")
-    public String list(@PathVariable("bid") String bid, Model model) {
+    public String list(@PathVariable("bid") String bid, @ModelAttribute BoardSearch search, Model model) {
         return utils.tpl("board/list");
     }
 
+    // 게시글 작성
     @GetMapping("/write/{bid}")
     public String write(@PathVariable("bid") String bid, Model model) {
 
         return utils.tpl("board/write");
     }
 
-    public void commonProcess() {
+    // 게시글 수정
+    @GetMapping("/update/{seq}")
+    public String update(@PathVariable("seq") Long seq, Model model) {
+
+        return utils.tpl("board/update");
+    }
+
+    // 게시글 보기
+    @GetMapping("/view/{seq}")
+    public String view(@PathVariable("seq") Long seq, Model model) {
+
+        return utils.tpl("board/view");
+    }
+
+    // 게시글 삭제
+    @GetMapping("/delete/{seq}")
+    public String delete(@PathVariable("seq") Long seq, Model model) {
+
+        // 게시판 목록으로 이동하기 위해 bid 넣음
+        return "redirect:/board/list/";
+    }
+
+    private void commonProcess(String bid, String mode, Model model) {
 
     }
+
+    private void commonProcess(Long seq, String mode, Model model) {
+
+    }
+
 }
